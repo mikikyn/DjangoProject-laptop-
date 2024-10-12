7cecde38e3704b59fb084b25640e4423b6478396
miki <baizhigit.akaev@gmail.com>
2024-10-12 16:57:10 +0600

    ls1

--- DjangoProject/urls.py
+++ DjangoProject/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from posts.views import test_view, main_page_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test/', test_view),
+    path('', main_page_view)
 ]
